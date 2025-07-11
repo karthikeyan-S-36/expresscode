020f5e9633f7dcf958eaf49c159020e9e72f416b
karthikeyan-S-36 <karthikeyansri110@gmail.com>
2025-07-11 19:18:52 +0530

    init message

--- product.js
+++ product.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const PORT = process.env.PORT || 6500;
-const buddy = require('./buddy.json');
+const PORT = process.env.PORT || 3500;
 const mysql = require('mysql');
 
 app.use(express.json());
@@ -21,3 +20,42 @@ con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+app.post('/api/products', (req, res) => {
+    const products = Array.isArray(req.body) ? req.body : [req.body];
+    const values = products.map(product => [product.brand, product.model, product.price_in_rs, product.country]);
+
+    let sql = "insert into products(brand,model,price_in_rs,country) values ?";
+    con.query(sql, [values], (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+app.get('/api/products/', (req, res) => {
+    const { user_id } = req.query;
+
+    if (isNaN(user_id)) {
+        res.send({ message: `${user_id} ID is not valid` });
+    } else {
+        let userTableSql = `select * from users where id = ${user_id}`;
+        con.query(userTableSql, (err, result) => {
+            if (err) {
+                res.send({ message: `${err}` });
+            }
+            else if (result.length == 0) {
+                res.send({ message: `User not found with ID ${user_id}` });
+            }
+            else {
+                let productTableSql = `select * from products`;
+                con.query(productTableSql, (err, result) => {
+                    if (err) throw err;
+                    res.send(result);
+                });
+            }
+        });
+    }
+
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
